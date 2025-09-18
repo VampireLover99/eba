9d856186a8414f5c01f5a81bc73446c58ceb1f1c
VampireLover99 <sophia.garbin.santos@escola.pr.gov.br>
2025-09-18 11:08:00 -0300

    eba

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+var nomeCantina = "Cantina da Escola";
+console.log("Bem-vindo à " + nomeCantina);
+Use para declarar a quantidade de salgados disponíveis (esse valor pode mudar).
+let salgados = 20;
+console.log("Temos " + salgados + " salgados disponíveis.");
+Use para declarar o preço fixo de cada salgado.
+const precoSalgado = 5;
+console.log("Cada salgado custa R$" + precoSalgado);
